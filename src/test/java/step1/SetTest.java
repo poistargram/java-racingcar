c13ea4d53f544054b0b5fad307243b80ad480df4
poistargram <poi.stargram@gamil.com>
2022-12-29 09:16:58 +0900

    Set의 size를 확인하는 테스트 작성

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,34 @@
+package step1;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("Set의 크기를 확인하는 테스트")
+	void size(){
+		int expectedSize = numbers.size();
+
+		assertThat(numbers.size()).isEqualTo(expectedSize);
+	}
+
+}
